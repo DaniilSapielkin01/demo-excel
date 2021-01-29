cf8b33c2e81db9c11487145a24fdf1381b37edc1
Daniil_Sapielkin <vipdanik98@gmail.com>
2021-01-30 01:05:35 +0200

    add selectGroup functional

--- src/core/ExcelComponent.js
+++ src/core/ExcelComponent.js
@@ -1,10 +1,15 @@
 import {DomListener} from "@core/DomListener";
 
 export class ExcelComponent extends DomListener {
-  constructor($root,options = {}) {
+  constructor($root, options = {}) {
     super($root, options.listeners)
     this.name = options.name || ''
 
+    this.prepare()
+
+  }
+
+  prepare() {
   }
 
   //Возвращает шаблон компонента
