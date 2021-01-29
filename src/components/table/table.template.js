520dbb230be645870b7d7c613e662ae7f939c6ea
Daniil_Sapielkin <vipdanik98@gmail.com>
2021-01-29 18:59:09 +0200

    Finish table resize

--- src/components/table/table.template.js
+++ src/components/table/table.template.js
@@ -3,34 +3,40 @@ const CODES = {
   Z: 90
 }
 
-function toCell() {
+function toCell(_, col) {
   return `
-    <div class="cell" contenteditable></div>
+    <div class="cell" contenteditable data-col="${col}"></div>
   `
 }
 
-function toColumn(col) {
-  return ` 
-    <div class="column">
-        ${col}
+function toColumn(col, index) {
+  return `
+    <div class="column" data-type="resizable" data-col="${index}" >
+      ${col}
+      <div class="col-resize" data-resize="col"></div>
     </div>
   `
 }
 
 function createRow(index, content) {
+  const resize = index ? '<div class="row-resize" data-resize="row" ></div>' : ''
   return `
-  <div class="row">
-   <div class="row-info">${index ? index : ""}</div>
-   <div class="row-data">${content}</div>
-  </div>`
+    <div class="row" data-type="resizable">
+      <div class="row-info">
+        ${index ? index : ''}
+        ${resize}
+      </div>
+      <div class="row-data">${content}</div>
+    </div>
+  `
 }
 
 function toChar(_, index) {
   return String.fromCharCode(CODES.A + index)
 }
 
-export function createTable(rowsCount = 10) {
-  const colsCount = CODES.Z - CODES.A + 1
+export function createTable(rowsCount = 15) {
+  const colsCount = CODES.Z - CODES.A + 1 // Compute cols count
   const rows = []
 
   const cols = new Array(colsCount)
@@ -45,11 +51,10 @@ export function createTable(rowsCount = 10) {
     const cells = new Array(colsCount)
        .fill('')
        .map(toCell)
-       .join("")
+       .join('')
+
     rows.push(createRow(i + 1, cells))
   }
 
-
   return rows.join('')
 }
-
